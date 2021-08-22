gifyic/android_device_xiaomi_sdm439-common

sdm439-common: Specify AVB recovery flags
Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
Change-Id: I576883b03b9c4239f9010bbc200179958e6fe95c

BoardConfigCommon.mk
@@ -12,6 +12,10 @@ COMMON_PATH := device/xiaomi/sdm439-common
 # AVB
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml

Krazy-Goel/device_xiaomi_olives-common

BoardConfigCommon.mk
@@ -68,6 +68,10 @@ USE_XML_AUDIO_POLICY_CONF := 1
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8937