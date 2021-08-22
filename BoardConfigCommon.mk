Xiaomi-SDM439/android_device_xiaomi_sdm439-common

sdm439-common: releasetools: Add dtbo image to the output zip if present
Signed-off-by: Dhina17 <dhinalogu@gmail.com>
Change-Id: Ifcfe6ca8adc01fc155804563ab19e8353d3cd9c7

BoardConfigCommon.mk
@@ -182,6 +182,9 @@ TARGET_PER_MGR_ENABLED := true
 # Protobuf
 PROTOBUF_SUPPORTED := false
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
+
 # Seecomp
 BOARD_SECCOMP_POLICY := $(VENDOR_PATH)/seccomp
 

Krazy-Goel/device_xiaomi_olives-common

BoardConfigCommon.mk
@@ -167,6 +167,9 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
+
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor