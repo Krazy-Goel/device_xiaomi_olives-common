22c894e43e350270279ac56e8ec755e3981efcd4
skittels15 <skittels1510@mail.ru>
2021-12-25 12:05:38 +0300

    sdm439-common: Footej changed to GCamGO

--- sdm439.mk
+++ sdm439.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libstdc++.vendor \
-    FootejCamera
+    GCamGOPrebuilt
 
 PRODUCT_PACKAGES += \
     libwa_dof
